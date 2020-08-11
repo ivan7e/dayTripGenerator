3b08e424a500155aa4db1e76741493e79558dde3
ivan7e <66914507+ivan7e@users.noreply.github.com>
2020-08-11 19:51:39 -0400

    Update dayTrip.js

--- dayTrip.js
+++ dayTrip.js
@@ -1,5 +1,5 @@
 function capFirst(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getRandomInt(min, max) {
@@ -12,24 +12,29 @@ function cityName(){
     let index = getRandomInt(0, 8);
     console.log(cityName[index]);
 
-function resturantName
+function resturantName(){
 
     var resturantName = ["APPLE BEES","OLIVE GARDEN","5 GUYS","RED LOBSTER","BUFFALO WILD WINGS","SUBWAY","OUTBACK STEAK HOUSE"];
     let index = getRandomInt(0, 6);
-    console.log(random.resturantName)
+    console.log(resturantName[index]);
 
-function transportationMethod
+function transportationMethod(){
 
     var transportationMethod = ["CAR","BUS","TRAIN","UBER","BICYCLE"];
     let index = getRandomInt(0, 5);
-    console.log(random.transportationMethod)
+    console.log(transportationMethod[index]);
 
-function entertainmentForm
+function entertainmentForm(){
    
-var entertainmentForm = ["STRIP CLUB","CASINO","BREWERY","PRO SPORTING EVENT","SHOPPING","NIGHT CLUB","CITY TOUR","HIKING"];
+    var entertainmentForm = ["STRIP CLUB","CASINO","BREWERY","PRO SPORTING EVENT","SHOPPING","NIGHT CLUB","CITY TOUR","HIKING"];
     let index = getRandomInt (0,7)
-    console.log(random.entertainmentForm)
+    console.log(entertainmentForm)[index]);
+
 
     
     
 }
+//change to one function 
+//update console.log 
+//use getRandomInt
+//test code repeat
