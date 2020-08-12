5e1d8b51e902c7b53de63d5b67ca4425fb8023c3
ivan7e <66914507+ivan7e@users.noreply.github.com>
2020-08-11 20:11:58 -0400

    Closing brackets
    Fix brackets and closed each line

--- dayTrip.js
+++ dayTrip.js
@@ -10,25 +10,25 @@ function cityName(){
 
     var cityName = ["NYC","BALTIMORE","PHILLY","WASHINGTON D.C.","SAN DIEGO","AUSTIN","L.A.","MIAMI","SEATLE"];
     let index = getRandomInt(0, 8);
-    console.log(cityName[index]);
+    console.log(cityName[index]);}
 
 function resturantName(){
 
     var resturantName = ["APPLE BEES","OLIVE GARDEN","5 GUYS","RED LOBSTER","BUFFALO WILD WINGS","SUBWAY","OUTBACK STEAK HOUSE"];
     let index = getRandomInt(0, 6);
-    console.log(resturantName[index]);
+    console.log(resturantName[index]);}
 
 function transportationMethod(){
 
     var transportationMethod = ["CAR","BUS","TRAIN","UBER","BICYCLE"];
     let index = getRandomInt(0, 5);
-    console.log(transportationMethod[index]);
+    console.log(transportationMethod[index]);}
 
 function entertainmentForm(){
    
     var entertainmentForm = ["STRIP CLUB","CASINO","BREWERY","PRO SPORTING EVENT","SHOPPING","NIGHT CLUB","CITY TOUR","HIKING"];
     let index = getRandomInt (0,7)
-    console.log(entertainmentForm)[index]);
+    console.log(entertainmentForm)[index];
 
 
     
